a68cbb3d39a0b06cc07afe7485bd19c28fe84b73
winsan <zhangxiaochang0@sina.com>
2016-11-09 13:26:54 +0800

    finished, useing sessionStorage not php

--- server/checkVerify.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "common_fun.php";
-header("Content-Type: application/json;charset=utf-8");
-session_start();
-$verifyValue =  $_POST['verifyValue'];
-$resData = checkVerify($verifyValue);
-echo $resData;
